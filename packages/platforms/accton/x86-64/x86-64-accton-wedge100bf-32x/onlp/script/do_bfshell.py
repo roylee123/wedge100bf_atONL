a0d533128fc381f2fc6de7b347b9bfda20121065
roy_lee <roy_lee@accton.com>
2018-09-04 07:23:56 +0000

    Auxiliary script for onlp to access bfshell
    Signed-off-by: roy_lee <roy_lee@accton.com>

---
+++ packages/platforms/accton/x86-64/x86-64-accton-wedge100bf-32x/onlp/script/do_bfshell.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import sys
+import pexpect
+import getopt
+import os
+
+def usage():
+    print "usage: {} [-f command_file] [-a ipv4_addr] [-p port] [-c command [-c comand] ...]".format(__file__)
+
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "a:p:f:c:")
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err)  # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+
+    commands = []
+    bfshell_path = '/opt/bfn/install/bin/bfshell'
+    bfshell_cmd = [ bfshell_path ]
+    for o, a in opts:
+        if o == '-a':
+            bfshell_cmd.extend(['-a', a])
+        elif o == '-p':
+            bfshell_cmd.extend(['-p', a])
+        elif o == '-f':
+            bfshell_cmd.extend(['-f', a])
+        elif o == '-c':
+            commands.append(a)
+        else:
+            assert False, "unhandled option"
+
+    if commands:
+        prompt_list = ['bfshell> $', 'bf-sde\.*.*> $', 'pd-switch:\d+> $', pexpect.EOF]
+        child = pexpect.spawn(' '.join(bfshell_cmd))
+        child.expect(prompt_list)
+        for cmd in commands:
+            child.sendline(cmd)
+            child.expect(prompt_list)
+            for line in child.before.splitlines():
+                if line.strip() and line != cmd:
+                    print line
+
+    else:
+        os.execv(bfshell_path, bfshell_cmd)
+
+if __name__ == '__main__':
+    main()
+    
+
