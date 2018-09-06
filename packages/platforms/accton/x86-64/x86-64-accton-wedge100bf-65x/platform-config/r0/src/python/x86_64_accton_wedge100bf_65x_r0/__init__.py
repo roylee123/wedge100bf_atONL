bcef1999eb5e5531da2fb53da90ac1177edfd0a9
roy_lee <roy_lee@accton.com>
2018-09-06 16:01:25 +0800

    [wedge100bf-65x]Build and apply accton_wedge100bf_psensor driver. Signed-off-by: roy_lee <roy_lee@accton.com>

--- packages/platforms/accton/x86-64/x86-64-accton-wedge100bf-65x/platform-config/r0/src/python/x86_64_accton_wedge100bf_65x_r0/__init__.py
+++ packages/platforms/accton/x86-64/x86-64-accton-wedge100bf-65x/platform-config/r0/src/python/x86_64_accton_wedge100bf_65x_r0/__init__.py
@@ -8,24 +8,7 @@ class OnlPlatform_x86_64_accton_wedge100bf_65x_r0(OnlPlatformAccton,
     SYS_OBJECT_ID=".100.65.1"
 
     def baseconfig(self):
-        ########### initialize I2C bus 1 & bus 2 ###########
-        self.new_i2c_devices([
-         # initialize multiplexer (PCA9548)
-                ('pca9548', 0x70, 1),
-                ('pca9548', 0x71, 1),
-                ('pca9548', 0x72, 1),
-                ('pca9548', 0x73, 1),
-                ('pca9548', 0x74, 1),
-
-
-                # initialize multiplexer (PCA9548)
-                ('pca9548', 0x70, 2),
-                ('pca9548', 0x71, 2),
-                ('pca9548', 0x72, 2),
-                ('pca9548', 0x73, 2),
-                ('pca9548', 0x74, 2),
-
-                ('24c64', 0x50, 41),
-                ])
+        #subprocess.call('depmod', shell=True)
+        self.insmod('modprobe accton_wedge100bf_psensor')
 
         return True
